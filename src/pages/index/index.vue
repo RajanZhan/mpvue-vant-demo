0a6a4908e7bae26347bde739fa6d35a4d64459e8
rajan <rajan.zhan@foxmail.com>
2018-10-27 14:06:11 +0800

    测试demo

---
+++ src/pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="" >
+
+	<van-button @click="clickHandle" type="warning">vant 按钮组件 </van-button>
+	
+  </div>
+</template>
+
+<script>
+import Toast from '@/../static/vant/toast/toast';
+export default {
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {}
+    }
+  },
+
+  components: {
+
+  },
+
+
+  methods: {
+ 
+    clickHandle () {;
+		console.log("toast is ",Toast);
+		
+		Toast.success('成功文案');
+		console.log('clickHandle:', msg, ev)
+    }
+  },
+}
+</script>
